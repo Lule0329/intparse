0e8da7faaeac07c852748b91820c8748f8964092
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-02-13 09:56:36 +0100

    Lade till kommentarer på koden.

--- intparse/Form1.Designer.cs
+++ intparse/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -50,6 +51,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "button 1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,6 +61,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button 2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -68,6 +71,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "button 3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -77,12 +81,22 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "button 4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(166, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -102,6 +116,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- intparse/Form1.cs
+++ intparse/Form1.cs
@@ -16,5 +16,90 @@ namespace intparse
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+
+            // Kör loopen för varje bokstav för att kolla om det är en siffra eller bokstav
+            // genom att kolla ASCII värdet på talet. 
+            for(int i = 0; i < textBox1.Text.Length; i++)
+            {
+                total *= 10;
+                total += textBox1.Text[i] - 48;
+            }
+
+            label1.Text = total.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+            int numericValue = 0;
+
+            // Kör loopen för varje bokstav för att kolla om det är en siffra eller bokstav
+            // genom att kolla ASCII värdet av talet. Fast tar inte med bokstäver i output.
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (char.IsDigit(inputString[i]))
+                {
+                    numericValue = numericValue * 10 + (inputString[i] - '0');
+                }
+            }
+            label1.Text = numericValue.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+            int numericValue = 0;
+
+            // Kör loopen för varje bokstav för att kolla om det är en siffra eller bokstav
+            // genom att kolla ASCII värdet av talet. Fast den slutar om det är en bokstav i talet.
+            // T.ex 12ab34, blir 12.
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (char.IsDigit(inputString[i]))
+                {
+                    numericValue = numericValue * 10 + (inputString[i] - '0');
+                }
+                else // Så fort det kommer en bokstav så avslutas loopen
+                {
+                    break;
+                }
+            }
+            label1.Text = numericValue.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+            int numericValue = 0;
+            bool containsLetter = false;
+
+            // Kör loopen för varje bokstav för att kolla om det är en siffra eller bokstav
+            // genom att kolla ASCII värdet av talet. Fast om det är en bokstav i talet så sätter den output till 0.
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (char.IsDigit(inputString[i]))
+                {
+                    numericValue = numericValue * 10 + (inputString[i] - '0');
+                }
+                else // om det finns bokstäver så avbryter den
+                {
+                    containsLetter = true;
+                    break;
+                }
+            }
+            
+            
+            if (containsLetter)
+            {
+                // om containsLetter är sant så sätter den output till '0'.
+                numericValue = 0;
+            }
+
+            label1.Text = numericValue.ToString();
+        }
     }
 }
